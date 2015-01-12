ec609f575b36d1edd3d2ffb84d025b263705394f
Dj Walker-Morgan <dj@codepope.com>
2015-01-12 15:24:22 +0000

    Tidied formatting

--- js/transporterdriving1/file-plus-transformer/justnameemail.js
+++ js/transporterdriving1/file-plus-transformer/justnameemail.js
@@ -1,2 +1,4 @@
 
-module.exports = function(doc) { return _.pick(doc, ["id", "first_name", "last_name", "email"]) }
+module.exports = function(doc) {
+  return _.pick(doc, ["id", "first_name", "last_name", "email"])
+  }
