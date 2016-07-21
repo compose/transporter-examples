6a231cdbffc1727f872738a70a948176b916dd56
Andrew Schwartz <andrew@compose.io>
2016-07-21 13:01:47 -0400

    support ssl in seed transports

--- go/cmd/seed/seed.go
+++ go/cmd/seed/seed.go
@@ -7,20 +7,24 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strings"
 	"time"
 
 	"gopkg.in/mgo.v2"
 
+	_ "github.com/compose/transporter/pkg/adaptor/all"
 	"github.com/compose/transporter/pkg/events"
 	"github.com/compose/transporter/pkg/transporter"
 )
 
 var (
-	sourceUri     = flag.String("s", "mongodb://localhost/", "source mongo uri to connect to")
-	destUri       = flag.String("d", "mongodb://localhost/", "destination mongo uri to write documents to")
+	sourceURI     = flag.String("s", "mongodb://localhost/", "source mongo uri to connect to")
+	destURI       = flag.String("d", "mongodb://localhost/", "destination mongo uri to write documents to")
 	sourceDB      = flag.String("source-db", "", "the source namespace to copy")
 	destinationDB = flag.String("dest-db", "", "the destination namespace")
 	debug         = flag.Bool("v", false, "debug, dumps all the documents to stdout")
@@ -31,32 +35,83 @@ func init() {
 	flag.Parse()
 }
 
-func main() {
+func dPrintf(s string, args ...interface{}) {
+	if *debug {
+		fmt.Printf(s, args)
+	}
+}
 
-	sess, err := mgo.Dial(*sourceUri)
+func testConn(uri string, ssl bool) {
+	dialInfo, err := mgo.ParseURL(uri)
+	dialInfo.FailFast = true
+	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		if ssl {
+			dPrintf("dialing ssl")
+			tlsConfig := &tls.Config{}
+			tlsConfig.InsecureSkipVerify = true
+			return tls.Dial("tcp", addr.String(), tlsConfig)
+		}
+		dPrintf("dialing non-ssl %+v\n", dialInfo)
+		return net.Dial("tcp", addr.String())
+	}
+	sess, err := mgo.DialWithInfo(dialInfo)
 	if err != nil {
 		fmt.Println("Can't connect: " + err.Error())
 		os.Exit(1)
 	}
+	dPrintf("success")
 	sess.Close()
+}
+
+type logger struct{}
+
+func (l *logger) Output(calldepth int, s string) error {
+	fmt.Printf("[%d] %s\n", calldepth, s)
+	return nil
+}
+
+func main() {
+	mgo.SetDebug(*debug)
+	if *debug {
+		mgo.SetLogger(&logger{})
+	}
+	sourceSSL := strings.Contains(*sourceURI, "ssl=true")
+	if sourceSSL {
+		*sourceURI = strings.Replace(*sourceURI, "ssl=true", "", -1)
+	}
+	destSSL := strings.Contains(*destURI, "ssl=true")
+	if destSSL {
+		*destURI = strings.Replace(*destURI, "ssl=true", "", -1)
+	}
+	dPrintf("testing source %s - ssl: %v...\n", *sourceURI, sourceSSL)
+	testConn(*sourceURI, sourceSSL)
+	dPrintf("testing dest %s - ssl: %v...\n", *destURI, destSSL)
+	testConn(*destURI, destSSL)
 
 	if *sourceDB == "" || *destinationDB == "" {
 		fmt.Fprintln(os.Stderr, "source and destination database must be provided, exiting")
 		os.Exit(1)
 	}
 
-	srcNamespace := fmt.Sprintf("%s./.*/", *sourceDB)
+	sourceNamespace := fmt.Sprintf("%s./.*/", *sourceDB)
 	destNamespace := fmt.Sprintf("%s./.*/", *destinationDB)
-
+	sourceConfig := map[string]interface{}{"uri": *sourceURI, "namespace": sourceNamespace, "tail": false}
+	if sourceSSL {
+		sourceConfig["ssl"] = map[string]interface{}{"cacerts": []string{}} // empty cacerts forces insecure no verify
+	}
+	destConfig := map[string]interface{}{"uri": *destURI, "namespace": destNamespace, "bulk": *bulk}
+	if destSSL {
+		destConfig["ssl"] = map[string]interface{}{"cacerts": []string{}} // empty cacerts forces insecure no verify
+	}
+	dPrintf("creating transport")
 	source :=
-		transporter.NewNode(fmt.Sprintf("source-%s", *sourceDB), "mongo", map[string]interface{}{"uri": *sourceUri, "namespace": srcNamespace, "tail": false}).
-			Add(transporter.NewNode(fmt.Sprintf("dest-%s", *destinationDB), "mongo", map[string]interface{}{"uri": *destUri, "namespace": destNamespace, "bulk": *bulk}))
+		transporter.NewNode(fmt.Sprintf("source-%s", *sourceDB), "mongodb", sourceConfig).
+			Add(transporter.NewNode(fmt.Sprintf("dest-%s", *destinationDB), "mongodb", destConfig))
 
 	if *debug {
 		source.Add(transporter.NewNode("out", "file", map[string]interface{}{"uri": "stdout://"}))
 	}
-
-	pipeline, err := transporter.NewPipeline(source, events.NewJsonLogEmitter(), 2*time.Second, nil, 10*time.Second)
+	pipeline, err := transporter.NewPipeline(source, events.NewJSONLogEmitter(), 2*time.Second, nil, 10*time.Second)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
